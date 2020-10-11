15c0c05ba5ba650256fbd5bdceb83463215829ea
michedomingo <michedomingo@gmail.com>
2020-10-11 01:22:09 -0700

    Implement morgan logging middleware

--- TJs_Fans_v1/server.js
+++ TJs_Fans_v1/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 // Route files
 const products = require('./routes/products');
@@ -8,6 +9,11 @@ const products = require('./routes/products');
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+};
 
 // Mount routers
 app.use('/api/v1/products', products);
