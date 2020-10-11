2b7cff2a9dc5d021ab7ab7e9185c73acbb76c4ee
michedomingo <michedomingo@gmail.com>
2020-10-10 23:58:09 -0700

    Set up routes

--- TJs_Fans_v1/server.js
+++ TJs_Fans_v1/server.js
@@ -8,4 +8,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/api/v1/products', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all products' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show product ${req.params.id}` });
+});
+
+app.post('/api/v1/products', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Create new product' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update product ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete product ${req.params.id}` });
+});
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
