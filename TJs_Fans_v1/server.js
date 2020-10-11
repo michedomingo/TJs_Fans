f42cbe4c4a013b1840afe4737958ca2ad2c4eaf7
michedomingo <michedomingo@gmail.com>
2020-10-11 08:36:31 -0700

    Create global handler for any handled promise rejections

--- TJs_Fans_v1/server.js
+++ TJs_Fans_v1/server.js
@@ -24,4 +24,13 @@ app.use('/api/v1/products', products);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: $err.message`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
