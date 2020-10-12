45e3a4bd55d28d449e49f14fb0ac0a7134145376
michedomingo <michedomingo@gmail.com>
2020-10-11 22:33:18 -0700

    Create middleware/wrapper function for asynchronous calls to refactor try catch in each controller method

--- TJs_Fans_v1/controllers/stores.js
+++ TJs_Fans_v1/controllers/stores.js
@@ -1,91 +1,72 @@
 const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 const Store = require("../models/Store");
 
 // @desc    Get all stores
 // @route   GET /api/v1/stores
 // @access  Public
-exports.getStores = async (req, res, next) => {
-  try {
-    const stores = await Store.find();
+exports.getStores = asyncHandler(async (req, res, next) => {
+  const stores = await Store.find();
 
-    res.status(200).json({ success: true, count: stores.length, data: stores });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({ success: true, count: stores.length, data: stores });
+});
 
 // @desc    Get single Store
 // @route   GET /api/v1/stores/:id
 // @access  Public
-exports.getStore = async (req, res, next) => {
-  try {
-    const store = await Store.findById(req.params.id);
+exports.getStore = asyncHandler(async (req, res, next) => {
+  const store = await Store.findById(req.params.id);
 
-    if (!store) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: store });
-  } catch (err) {
-    next(err);
+  if (!store) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: store });
+});
 
 // @desc    Create new Store
 // @route   POST /api/v1/stores
 // @access  Private
-exports.createStore = async (req, res, next) => {
-  try {
-    const store = await Store.create(req.body);
+exports.createStore = asyncHandler(async (req, res, next) => {
+  const store = await Store.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: store,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: store,
+  });
+});
 
 // @desc    Update Store
 // @route   POST /api/v1/stores/:id
 // @access  Private
-exports.updateStore = async (req, res, next) => {
-  try {
-    const store = await Store.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+exports.updateStore = asyncHandler(async (req, res, next) => {
+  const store = await Store.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!store) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: store });
-  } catch (err) {
-    next(err);
+  if (!store) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: store });
+});
 
 // @desc    Delete Store
 // @route   POST /api/v1/stores/:id
 // @access  Private
-exports.deleteStore = async (req, res, next) => {
-  try {
-    const store = await Store.findByIdAndDelete(req.params.id);
+exports.deleteStore = asyncHandler(async (req, res, next) => {
+  const store = await Store.findByIdAndDelete(req.params.id);
 
-    if (!store) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    next(err);
+  if (!store) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+
+  res.status(200).json({ success: true, data: {} });
+});
