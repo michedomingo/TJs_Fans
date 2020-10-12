531647bcd72c48d8aeaedb37fd0223c4e4b35536
michedomingo <michedomingo@gmail.com>
2020-10-12 08:11:43 -0700

    Implement database seeder to import/delete data

---
+++ TJs_Fans_v1/seeder.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Load models
+const Store = require("./models/Store");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const stores = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/stores.json`, "utf-8")
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Store.create(stores);
+
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Store.deleteMany();
+
+    console.log("Data Destroyed...".red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
