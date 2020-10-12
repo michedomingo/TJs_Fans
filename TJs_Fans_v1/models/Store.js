295f48d65fc57d7b6657835cde1c01237816691a
michedomingo <michedomingo@gmail.com>
2020-10-12 00:07:00 -0700

    Implement GeoJSON location and Geocoder hook to use submitted address to create geo JSON field

--- TJs_Fans_v1/models/Store.js
+++ TJs_Fans_v1/models/Store.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const geocoder = require("../utils/geocoder");
 
 const StoreSchema = new mongoose.Schema({
   name: {
@@ -84,10 +85,29 @@ const StoreSchema = new mongoose.Schema({
   },
 });
 
-// Create bootcamp slug from the name
-BootcampSchema.pre("save", function (next) {
+// Create store slug from the name
+StoreSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// Geocode and create location field
+StoreSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+  next();
+});
+
+// Do not save address in DB
+this.address = undefined;
 
 module.exports = mongoose.model("Store", StoreSchema);
