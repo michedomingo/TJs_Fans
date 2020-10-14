efee078907b2615401dd48fa21d60be72727d3a7
michedomingo <michedomingo@gmail.com>
2020-10-13 19:46:55 -0700

    Get specific product(s) for a store

--- TJs_Fans_v1/routes/products.js
+++ TJs_Fans_v1/routes/products.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getProducts } = require("../controllers/products");
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router.route("/").get(getProducts);
 
