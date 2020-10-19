bf0aaba6be3095d6af44b3c31d1b4877d32876cd
michedomingo <michedomingo@gmail.com>
2020-10-19 13:22:30 -0700

    Update & delete reviews (pending debugging/refactor for previous errors

--- TJs_Fans_v1/controllers/reviews.js
+++ TJs_Fans_v1/controllers/reviews.js
@@ -67,3 +67,66 @@ exports.addReview = asyncHandler(async (req, res, next) => {
 
 // ErrorResponse: No store with the id of undefined
 //     at /Users/miche.flamingo/OneDrive/3_Programming/TJs_Fans/TJs_Fans_v1/controllers/reviews.js:56:7
+
+// @desc    Update review
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `Not authorized to update review with the id of ${req.params.id}`,
+        401
+      )
+    );
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+// @desc    Delete review
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `Not authorized to update review with the id of ${req.params.id}`,
+        401
+      )
+    );
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
