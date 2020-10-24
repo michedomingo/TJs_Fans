7d02fdc2cb94af82c8618c2eaf26f4c615c8d514
michedomingo <michedomingo@gmail.com>
2020-10-24 11:36:40 -0700

    Registration form and account activation with UI auth errors finally working!

---
+++ TJs_Fans_v1.1/client/src/components/Layout.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+const Layout = ({ children, match }) => {
+  const isActive = (path) => {
+    if (match.path === path) {
+      return { color: '#000' };
+    } else {
+      return { color: '#fff' };
+    }
+  };
+
+  const nav = () => (
+    <ul className='nav nav-tabs bg-primary'>
+      <li className='nav-item'>
+        <Link to='/' className='nav-link' style={isActive('/')}>
+          Home
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link to='/login' className='nav-link' style={isActive('/login')}>
+          Login
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link to='/register' className='nav-link' style={isActive('/register')}>
+          Register
+        </Link>
+      </li>
+    </ul>
+  );
+
+  return (
+    <Fragment>
+      {nav()}
+      <div className='container'>{children}</div>
+    </Fragment>
+  );
+};
+
+export default withRouter(Layout);
