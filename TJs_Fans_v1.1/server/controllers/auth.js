a4e8f74eee45c1e881f1d53f07eb2d8886ef2c44
michedomingo <michedomingo@gmail.com>
2020-10-23 18:59:38 -0700

    Express validator / register route now working

--- TJs_Fans_v1.1/server/controllers/auth.js
+++ TJs_Fans_v1.1/server/controllers/auth.js
@@ -1,8 +1,30 @@
-exports.register = (req, res) => {
-  console.log('REQ BODY ON SIGNUP', req.body);
+const User = require('../models/User');
 
-  res.status(200).json({
-    success: true,
-    msg: 'hit signup register endpoint - WOOT! CONTROLLER',
+exports.register = (req, res) => {
+  //   console.log('REQ BODY ON SIGNUP', req.body);
+
+  const { name, email, password } = req.body;
+
+  // Mongoose method
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: 'Email is already registered in the system',
+      });
+    }
+  });
+
+  let newUser = new User({ name, email, password });
+
+  newUser.save((err, success) => {
+    if (err) {
+      console.log('SIGNUP ERROR', err);
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    res.json({
+      message: 'Registration success! Please login',
+    });
   });
 };
--- TJs_Fans_v1.1/server/server.js
+++ TJs_Fans_v1.1/server/server.js
@@ -8,16 +8,16 @@ const connectDB = require('./config/db');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+const app = express();
+
 // Connect to database
 connectDB();
 
-const app = express();
+// Route files
+const authRoutes = require('./routes/auth');
 
 // Body parser
 app.use(express.json());
-
-// Route files
-const authRoutes = require('./routes/auth');
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -34,17 +34,9 @@ if ((process.env.NODE_ENV = 'development')) {
 app.use('/api/v1', authRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: $err.message`.red);
-  // Close server & exit process
-  server.close(() => process.exit(1));
-});
