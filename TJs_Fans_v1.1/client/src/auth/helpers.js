2b4033eab9206dae4e152e673ff1523fa81ba9c8
michedomingo <michedomingo@gmail.com>
2020-10-24 22:12:02 -0700

    Update profile in private component

--- TJs_Fans_v1.1/client/src/auth/helpers.js
+++ TJs_Fans_v1.1/client/src/auth/helpers.js
@@ -63,3 +63,13 @@ export const logout = (next) => {
   removeLocalStorage('user');
   next();
 };
+
+export const updateUser = (response, next) => {
+  console.log('UPDATE USER IN LOCALSTORAGE HELPERS', response);
+  if (typeof window !== 'undefined') {
+    let auth = JSON.parse(localStorage.getItem('user'));
+    auth = response.data;
+    localStorage.setItem('user', JSON.stringify(auth));
+  }
+  next();
+};
--- TJs_Fans_v1.1/client/src/components/Private.js
+++ TJs_Fans_v1.1/client/src/components/Private.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Layout from '../components/Layout';
 import axios from 'axios';
-import { isAuth } from '../auth/helpers';
+import { isAuth, getCookie, logout, updateUser } from '../auth/helpers';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
-const Private = () => {
+const Private = ({ history }) => {
   const [values, setValues] = useState({
     role: '',
     name: '',
@@ -14,6 +14,35 @@ const Private = () => {
     password: '',
     buttonText: 'Submit',
   });
+
+  const token = getCookie('token');
+
+  useEffect(() => {
+    loadProfile();
+  }, []);
+
+  const loadProfile = () => {
+    axios({
+      method: 'GET',
+      url: `${process.env.REACT_APP_API}/user/${isAuth()._id}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        console.log('PRIVATE PROFILE UPDATE', response);
+        const { role, name, email } = response.data;
+        setValues({ ...values, role, name, email });
+      })
+      .catch((error) => {
+        console.log('PRIVATE PROFILE UPDATE ERROR', error.response.data.error);
+        if (error.response.status === 401) {
+          logout(() => {
+            history.push('/');
+          });
+        }
+      });
+  };
 
   const { role, name, email, password, buttonText } = values;
 
@@ -26,23 +55,25 @@ const Private = () => {
     event.preventDefault();
     setValues({ ...values, buttonText: 'Submitting' });
     axios({
-      method: 'POST',
-      url: `${process.env.REACT_APP_API}/signup`,
-      data: { name, email, password },
+      method: 'PUT',
+      url: `${process.env.REACT_APP_API}/user/update`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      data: { name, password },
     })
       .then((response) => {
-        console.log('SIGNUP SUCCESS', response);
-        setValues({
-          ...values,
-          name: '',
-          email: '',
-          password: '',
-          buttonText: 'Submitted',
+        console.log('PRIVATE PROFILE UPDATE SUCCESS', response);
+        updateUser(response, () => {
+          setValues({
+            ...values,
+            buttonText: 'Submitted',
+          });
+          toast.success('Profile updated successfully');
         });
-        toast.success(response.data.message);
       })
       .catch((error) => {
-        console.log('SIGNUP ERROR', error.response.data);
+        console.log('PRIVATE PROFILE UDPATE ERROR', error.response.data.error);
         setValues({ ...values, buttonText: 'Submit' });
         toast.error(error.response.data.error);
       });
@@ -52,7 +83,12 @@ const Private = () => {
     <form>
       <div className='form-group'>
         <label className='text-muted'>Role</label>
-        <input defaultValue={role} type='text' className='form-control' />
+        <input
+          defaultValue={role}
+          type='text'
+          className='form-control'
+          disabled
+        />
       </div>
       <div className='form-group'>
         <label className='text-muted'>Name</label>
@@ -66,7 +102,12 @@ const Private = () => {
 
       <div className='form-group'>
         <label className='text-muted'>Email</label>
-        <input defaultValue={email} type='email' className='form-control' />
+        <input
+          defaultValue={email}
+          type='email'
+          className='form-control'
+          disabled
+        />
       </div>
 
       <div className='form-group'>
